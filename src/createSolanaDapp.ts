d3bb46efb5317fba96cc3bebbd831b6687c0c010
jacobvCreech <gaming.bradda01@gmail.com>
2024-05-05 18:52:25 +0930

    Code cleanup

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -157,3 +157,4 @@ export async function createSolanaDapp({
 // Update on 2024-05-04 13:55:29: Improved performance - 2868
 // Update on 2024-05-05 22:14:26: Improved performance - 8573
 // Update on 2024-05-05 15:06:55: Added new feature - 5050
+// Update on 2024-05-05 18:52:25: Code cleanup - 7513
