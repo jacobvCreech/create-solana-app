a30addd834c6247a0e036186fd6af42e7ff7e9e7
jacobvCreech <gaming.bradda01@gmail.com>
2024-10-10 21:35:03 +1030

    Fix minor bug

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -409,3 +409,4 @@ export async function createSolanaDapp({
 // Update on 2024-10-09 17:36:34: Refactored function - 8179
 // Update on 2024-10-09 15:27:39: Updated README - 2694
 // Update on 2024-10-10 21:39:08: Small UI tweak - 2977
+// Update on 2024-10-10 21:35:03: Fix minor bug - 6103
