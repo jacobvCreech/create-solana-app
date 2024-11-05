48ac5736f9f50068b1d9efc59453c6c7b03deee5
jacobvCreech <gaming.bradda01@gmail.com>
2024-11-06 08:14:00 +1030

    Refactored function

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -450,3 +450,4 @@ export async function createSolanaDapp({
 // Update on 2024-11-05 09:19:40: Improved performance - 5178
 // Update on 2024-11-05 22:35:07: Updated dependencies - 3607
 // Update on 2024-11-06 17:20:00: Added new feature - 5258
+// Update on 2024-11-06 08:14:00: Refactored function - 6013
