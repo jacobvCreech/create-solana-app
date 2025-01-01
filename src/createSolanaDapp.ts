3d4f34ada50fc47ae1030b59173143a46fa06948
jacobvCreech <gaming.bradda01@gmail.com>
2025-01-01 11:34:40 +1030

    Updated README

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -535,3 +535,4 @@ export async function createSolanaDapp({
 // Update on 2024-12-31 16:34:20: Code cleanup - 2568
 // Update on 2025-01-01 14:56:17: Updated README - 3552
 // Update on 2025-01-01 09:06:43: Updated README - 9205
+// Update on 2025-01-01 11:34:40: Updated README - 5280
