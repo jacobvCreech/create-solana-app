a2991ee61456104cf00d22109fcb0bc7f554645a
jacobvCreech <gaming.bradda01@gmail.com>
2025-01-29 08:13:57 +1030

    Improved performance

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -571,3 +571,4 @@ export async function createSolanaDapp({
 // Update on 2025-01-28 19:38:57: Refactored some code - 7855
 // Update on 2025-01-28 15:04:20: Added new feature - 7515
 // Update on 2025-01-29 20:06:54: Updated dependencies - 7785
+// Update on 2025-01-29 08:13:57: Improved performance - 9204
