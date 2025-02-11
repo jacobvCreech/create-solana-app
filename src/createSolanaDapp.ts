ecb8b6a9e27276ac39fd69c4f38fe1ae2e8e3760
jacobvCreech <gaming.bradda01@gmail.com>
2025-02-11 16:13:24 +1030

    Small UI tweak

--- src/createSolanaDapp.ts
+++ src/createSolanaDapp.ts
@@ -593,3 +593,4 @@ export async function createSolanaDapp({
 // Update on 2025-02-09 22:20:13: Improved performance - 4531
 // Update on 2025-02-09 14:22:51: Updated dependencies - 9227
 // Update on 2025-02-10 18:17:56: Code cleanup - 9957
+// Update on 2025-02-11 16:13:24: Small UI tweak - 7812
